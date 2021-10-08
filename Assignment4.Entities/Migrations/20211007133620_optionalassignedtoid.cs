98c7d3e251d55dc9bedf27e58e92591b2fe26dca
Adrian Borup <avborup@gmail.com>
2021-10-08 14:57:08 +0200

    Add migrations

---
+++ Assignment4.Entities/Migrations/20211007133620_optionalassignedtoid.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment4.Entities.Migrations
+{
+    public partial class optionalassignedtoid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedToId",
+                table: "Tasks",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks",
+                column: "AssignedToId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedToId",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks",
+                column: "AssignedToId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
