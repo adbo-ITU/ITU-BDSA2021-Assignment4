4dde7d75f44b5de2d97f123e8db05808be17e376
coha <coha@itu.dk>
2021-10-11 17:45:09 +0200

    test and working User bacis. not tested

--- Assignment4.Entities.Tests/UserRepositoryTests.cs
+++ Assignment4.Entities.Tests/UserRepositoryTests.cs
@@ -1,10 +1,118 @@
 using System;
 using Xunit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Assignment4;
+using System.IO;
+using System.Linq;
+using Assignment4.Core;
+using System.Collections.Generic;
+using Assignment4.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Assignment4.Entities.Tests
 {
     public class UserRepositoryTests
     {
+        private readonly KanbanContext _context;
+        private readonly UserRepository _repo;
+        public UserRepositoryTests()
+        {
+            
+         var configuration = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddUserSecrets<TaskRepositoryTests>()
+         .Build();
+         
+         var connectionString = configuration.GetConnectionString("bdsa-kanban");
+         var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseNpgsql(connectionString);
+         var context = new KanbanContext(optionsBuilder.Options);
+         _context.RemoveAllData();
+         _repo = new UserRepository(_context);
 
+
+  
+        }
+
+        public void Dispose()
+        {
+            _context.RemoveAllData();
+            _context.Dispose();
+        }
+        public int CreateUser()
+        {
+            var newUser = new User { Name = "ole", Email = "ole@itu.dk" };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            return newUser.Id;
+        }
+
+        [Fact]
+        public void Delete_a_user_using_force_returns_Deleted()
+        {
+            //Given
+            var id = CreateUser();
+            
+       
+            //When
+            var response = _repo.Delete(id);
+            //Then
+
+            Assert.Equal(Response.Conflict,response);
+        }
+
+        [Fact]
+        public void Delete_a_user_not_using_force_returns_Conflict()
+        {
+            throw new System.NotImplementedException();
+             //Given
+            var id = CreateUser();
+            
+       
+            //When
+            var response = _repo.Delete(id,force: true);
+            //Then
+
+            Assert.Equal(Response.Deleted,response);
+        }
+        [Fact]
+        public void Delete_a_user_not_exsting_returns_NotFound()
+        {
+              throw new System.NotImplementedException();
+            var id = CreateUser()+1;
+            
+       
+            //When
+            var response = _repo.Delete(id);
+            //Then
+
+            Assert.Equal(Response.NotFound,response);
+        }
+
+         [Fact]
+        public void Create_a_user()
+        {
+
+            //Act
+           var (respons,userId) = _repo.Create(new UserCreateDTO{Name = "ole", Email = "ole@itu.dk"});
+            // Assert
+            Assert.Equal(Response.Created,respons);
+
+        }
+         public void Create_a_allready_exsisting_user()
+        {
+               // Arrange
+            var newUser = new User { Name = "ole", Email = "ole@itu.dk" };
+             _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            //Act
+           var (respons,userId) = _repo.Create(new UserCreateDTO{Name = "ole", Email = "ole@itu.dk"});
+            // Assert
+            Assert.Equal(Response.Conflict,respons);
+
+
+        }
+    
     }
 }
--- Assignment4.Entities/UserRepository.cs
+++ Assignment4.Entities/UserRepository.cs
@@ -1,6 +1,70 @@
+using System.Collections.Generic;
+using System.IO;
+using Assignment4.Core;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
 namespace Assignment4.Entities
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private KanbanContext _context;
+
+        public UserRepository(KanbanContext context)
+        {
+            _context = context;
+        }
+
+
+        public (Response Response, int UserId) Create(UserCreateDTO user)
+        {
+            var existingUserWithEmail = from u in _context.Users
+                                        where u.Email == user.Email
+                                        select u.Id;
+            if (existingUserWithEmail!=null){
+                return ( Response.Conflict,existingUserWithEmail.First());
+            }
+
+             var newUser = new User { Name = user.Name, Email = user.Email };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            return( Response.Created,newUser.Id);
+
+        }
+
+        public Response Delete(int UserId, bool force = false)
+        {
+            var user = _context.Users.Find(UserId);
+            if(user == null) return Response.NotFound;
+            if(!force) return Response.Conflict;
+            _context.Users.Remove(user);
+            return Response.Deleted;
+        }
+
+        public UserDTO Read(int UserId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IReadOnlyCollection<TagDTO> ReadAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public Response Update(UserUpdateDTO user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        UserDTO IUserRepository.Read(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IReadOnlyCollection<UserDTO> IUserRepository.ReadAll()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
